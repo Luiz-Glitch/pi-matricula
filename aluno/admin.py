cec3f963c5b779024989c536408910fd3302fe40
Luiz Bessa <lbessaregis@gmail.com>
2024-09-02 07:52:33 -0300

    add model

--- aluno/admin.py
+++ aluno/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
+from .models import Aluno
+
 # Register your models here.
+admin.register(Aluno)
+class AlunoAdmin(admin.ModelAdmin):
+    list_display = ('nome', 'matricula', 'email', 'curso')
+    list_filter = ('nome', 'matricula', 'email', 'curso')
--- aluno/models.py
+++ aluno/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class Aluno(models.Model):
+    CURSO_CHOICES = (
+        ('INFORMATICA', 'Informática'),
+        ('ALIMENTOS', 'Alimentos'),
+        ('APICULTURA', 'APICULTURA'),
+    )
+    
+    nome = models.CharField(max_length=255)
+    matricula = models.CharField(max_length=255)
+    email = models.EmailField()
+    endereco = models.CharField(max_length=255)
+    curso = models.CharField(max_length=255, choices=CURSO_CHOICES)
